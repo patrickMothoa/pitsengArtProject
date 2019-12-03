a98c8d5a0448e61fff84ad45eca29e3ee6e1658b
449sharon <50619633+449sharon@users.noreply.github.com>
2019-12-03 10:29:06 +0200

    frond end home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,14 +11,8 @@ const routes: Routes = [
   {
     path: 'details',
     loadChildren: () => import('./pages/details/details.module').then( m => m.DetailsPageModule)
-  },  {
-    path: 'login',
-    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
   },
-  {
-    path: 'register',
-    loadChildren: () => import('./pages/register/register.module').then( m => m.RegisterPageModule)
-  },
+  
 
 
 ];
